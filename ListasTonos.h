d5474716f9531d1bc2dbec0a65965e12d59c91ec
Goyeneche23 <85038931+Goyeneche23@users.noreply.github.com>
2024-09-20 09:39:04 -0400

    Update ListasTonos.h

--- ListasTonos.h
+++ ListasTonos.h
@@ -1,9 +1,13 @@
 #ifndef LISTASTONOS_H
 #define LISTASTONOS_H
 
-vector<int> escalaMayor = {0, 2, 4, 5, 7, 9, 11};  // C, D, E, F, G, A, B
-vector<int> escalaMenorNatural = {0, 2, 3, 5, 7, 8, 10};  // A, B, C, D, E, F, G
+class Tonos{
+    private:
+        vector<int> escalaMayor = {0, 2, 4, 5, 7, 9, 11};  // C, D, E, F, G, A, B
+        vector<int> escalaMenorNatural = {0, 2, 3, 5, 7, 8, 10};  // A, B, C, D, E, F, G
+        vector<int> 
+    public:
 
-
+}
 
 #endif
